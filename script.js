eca3232c1d49853f0acc89ba4d71afb7b3479ea8
Rishuraj Singh <102401151+rraawww10@users.noreply.github.com>
2024-10-28 10:53:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 function firstWord(s) {
   // your code here
+		if(s === ''){
+		return '';
+	}
+	let firstSpace = s.indexOf(' ');
+    if(firstSpace === -1){
+		return s;
+	}
+	
+	return s.slice(0 , firstSpace);
 }
+
 
 // Do not change the code below
 
