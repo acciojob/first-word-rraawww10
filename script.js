b3c2da172842157a29db8d90a5cc37ff46460c06
Rishuraj Singh <102401151+rraawww10@users.noreply.github.com>
2024-11-06 10:21:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,8 @@
-function firstWord(s) {
-  // your code here
-		if(s === ''){
-		return '';
-	}
-	let firstSpace = s.indexOf(' ');
-    if(firstSpace === -1){
-		return s;
-	}
-	
-	return s.slice(0 , firstSpace);
+function firstWord(str) {
+    let trimmedStr = str.trim();
+    let spaceIndex = trimmedStr.indexOf(' ');
+    return spaceIndex === -1 ? trimmedStr : trimmedStr.slice(0, spaceIndex);
 }
+const s = prompt("Enter a string");
 
-
-// Do not change the code below
-
-const s = prompt("Enter String:");
 alert(firstWord(s));
